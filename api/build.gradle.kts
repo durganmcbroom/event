5bb6c9954e0e6e822205dd933dbbe6008da3874c
durganmcbroom <durganmcbroom@gmail.com>
2024-07-01 17:17:40 -0700

    Publish not working -> publication defined by 'withType' not getting crated, replace with 'create'

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -12,36 +12,38 @@ task<Jar>("javadocJar") {
 }
 
 publishing {
-    publications.withType<MavenPublication> {
-        from(components["java"])
-        artifact(tasks["sourcesJar"])
-        artifact(tasks["javadocJar"])
+    publications {
+        create("api", MavenPublication::class) {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
 
-        pom {
-            name.set("Event Api")
-            description.set("An Api for receiving, dispatching, transforming all other things related to events!")
-            url.set("https://github.com/durganmcbroom/event")
-
-            packaging = "jar"
-
-            developers {
-                developer {
-                    id.set("durganmcbroom")
-                    name.set("Durgan McBroom")
-                }
-            }
-
-            licenses {
-                license {
-                    name.set("MIT License")
-                    url.set("https://opensource.org/licenses/MIT")
-                }
-            }
-
-            scm {
-                connection.set("scm:git:git://github.com/durganmcbroom/event")
-                developerConnection.set("scm:git:ssh://github.com:durganmcbroom/event.git")
+            pom {
+                name.set("Event Api")
+                description.set("An Api for receiving, dispatching, transforming all other things related to events!")
                 url.set("https://github.com/durganmcbroom/event")
+
+                packaging = "jar"
+
+                developers {
+                    developer {
+                        id.set("durganmcbroom")
+                        name.set("Durgan McBroom")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/durganmcbroom/event")
+                    developerConnection.set("scm:git:ssh://github.com:durganmcbroom/event.git")
+                    url.set("https://github.com/durganmcbroom/event")
+                }
             }
         }
     }
--- fsm/build.gradle.kts
+++ fsm/build.gradle.kts
@@ -20,36 +20,38 @@ task<Jar>("javadocJar") {
 }
 
 publishing {
-    publications.withType<MavenPublication> {
-        from(components["java"])
-        artifact(tasks["sourcesJar"])
-        artifact(tasks["javadocJar"])
+    publications {
+        create("api", MavenPublication::class) {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
 
-        pom {
-            name.set("Event FSM")
-            description.set("An extension of the Event API to create and use finite state machines for events.")
-            url.set("https://github.com/durganmcbroom/event")
-
-            packaging = "jar"
-
-            developers {
-                developer {
-                    id.set("durganmcbroom")
-                    name.set("Durgan McBroom")
-                }
-            }
-
-            licenses {
-                license {
-                    name.set("MIT License")
-                    url.set("https://opensource.org/licenses/MIT")
-                }
-            }
-
-            scm {
-                connection.set("scm:git:git://github.com/durganmcbroom/event")
-                developerConnection.set("scm:git:ssh://github.com:durganmcbroom/event.git")
+            pom {
+                name.set("Event FSM")
+                description.set("An extension of the Event API to create and use finite state machines for events.")
                 url.set("https://github.com/durganmcbroom/event")
+
+                packaging = "jar"
+
+                developers {
+                    developer {
+                        id.set("durganmcbroom")
+                        name.set("Durgan McBroom")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/durganmcbroom/event")
+                    developerConnection.set("scm:git:ssh://github.com:durganmcbroom/event.git")
+                    url.set("https://github.com/durganmcbroom/event")
+                }
             }
         }
     }
